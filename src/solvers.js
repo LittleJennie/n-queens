c75f1ac9287ef0abb3b6cc5f68a93014e835e9cd
Jennie Zeng <jenniezeng1@gmail.com>
2019-06-06 22:58:49 -0700

    implement bit map approach for n-rook

--- src/solvers.js
+++ src/solvers.js
@@ -34,32 +34,22 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var chessBoard = new Board({'n': n});
-  var chessCount = 0;
+  // use bit map to implement the count solution method
 
-  // we will use this function recursively to add chess to board
-  // we will apply the hasConflict method from Board.js to check if current move is valid before moving on to next step
+  var colMarker = new Array(n).fill(0);
+
   var findSolutionCount = function (row) {
     var hasOneSolution = 0;
-    if (row >= n && chessCount === n) {
-      hasOneSolution = 1;
-      return hasOneSolution;
-    }
-
-    if (row >= n && chessCount !== n) {
-      hasOneSolution = 0;
-      return hasOneSolution;
+    if (row >= n ) {
+      return 1;
     }
 
     for (var col = 0; col < n; col ++) {
-      chessBoard.togglePiece(row, col);
-      chessCount++;
-      if (!chessBoard.hasAnyRooksConflicts()) {
-        var newRow = row + 1;
-        hasOneSolution += findSolutionCount(newRow);
+      if (colMarker[col] === 0) {
+        colMarker[col] = 1;
+        hasOneSolution += findSolutionCount(row + 1);
+        colMarker[col] = 0;
       }
-      chessBoard.togglePiece(row, col);
-      chessCount--;
     }
 
     return hasOneSolution;
@@ -83,39 +73,29 @@ window.findNQueensSolution = function(n) {
     return [[1]];
   }
 
-  var findSolutionCount = function (row) {
-    
-    var hasOneSolution = [];
+  var findSolution = function (row) {
     if (row >= n && chessCount === n) {
-      var curSolutionBoard = chessBoard.rows();
-      // hasOneSolution = 1;
-      var ansArr = JSON.stringify(curSolutionBoard);
-      // console.log(ansArr)
-      return ansArr;
-    }
-
-    if (row >= n && chessCount !== n) {
-      // hasOneSolution = 0;
-      return [];
+      return chessBoard.rows();
     }
 
     for (var col = 0; col < n; col ++) {
       chessBoard.togglePiece(row, col);
       chessCount++;
       if (!chessBoard.hasAnyQueensConflicts()) {
-        var newRow = row + 1;
-        hasOneSolution = hasOneSolution.concat(findSolutionCount(newRow));
+        var result = findSolution(row + 1);
+        if (result !== null) {
+          return result;
+        }
       }
       chessBoard.togglePiece(row, col);
       chessCount--;
     }
-    return hasOneSolution;
+    return null;
   }
 
-  var solution = findSolutionCount(0); 
-  solution = solution.pop();
-  console.log(solution, "here")
-  solution = JSON.parse(solution);
+  var solution = findSolution(0); 
+  // solution = solution.pop();
+  // solution = JSON.parse(solution);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -124,83 +104,28 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {  
   var chessBoard = new Board({'n': n});
-  var solution = 0;
-  var chessCount = 0;
 
   // we will use this function recursively to add chess to board
   // we will apply the hasConflict method from Board.js to check if current move is valid before moving on to next step
   var findSolutionCount = function (row) {
     var hasOneSolution = 0;
-    if (row >= n && chessCount === n) {
-      hasOneSolution = 1;
-      return hasOneSolution;
-    }
-
-    if (row >= n && chessCount !== n) {
-      hasOneSolution = 0;
-      return hasOneSolution;
+    if (row >= n ) {
+      return 1;
     }
 
     for (var col = 0; col < n; col ++) {
       chessBoard.togglePiece(row, col);
-      chessCount++;
       if (!chessBoard.hasAnyQueensConflicts()) {
-        var newRow = row + 1;
-        hasOneSolution += findSolutionCount(newRow);
+        hasOneSolution += findSolutionCount(row + 1);
       }
       chessBoard.togglePiece(row, col);
-      chessCount--;
     }
 
     return hasOneSolution;
   }
 
+  var solution = findSolutionCount(0);
+
   console.log('Number of solutions for ' + n + ' queens:', solution);
-  return findSolutionCount(0);
+  return solution;
 };
-
-
-  // try 4
-  // window.countNQueensSolutions = function(n) {  
-  //   var chessBoard = new Board({'n': n});
-  //   var solution = 0;
-  //   var chessCount = 0;
-  //   // helper function should take __ arguments
-  //   // 1. n
-  //   // 2. rowCount
-  //   // 3. total onBoardChessCount
-  //   // 4. paintedChessBoard
-  //   // 5. solutionCount
-  
-  //   // we will use this function recursively to add chess to board
-  //   // we will apply the hasConflict method from Board.js to check if current move is valid before moving on to next step
-  //   var findSolutionCount = function (row = 0, col = 0) {
-  //     if (row === n - 1 && col === n - 1 && chessCount === n) {
-  //       return 1;
-  //     }
-  //     if (row === n - 1 && col === n - 1 && chessCount !== n) {
-  //       return 0;
-  //     }
-  //     for (var i = row; i < n; i++) {
-  //       for (var j = col; j < n; j++) {
-  //         chessBoard.togglePiece(row, col);
-  //         if (!chessBoard.hasAnyQueensConflicts()) { // if the current placement is valid
-  //           if (col === n - 1) {
-  //             solution += findSolutionCount(++row, 0);
-  //           } else {
-  //             solution += findSolutionCount(row, ++col);
-  //           }
-  //         } else {
-  //           chessBoard.togglePiece(row, col);
-  //         }
-  //       }
-  //     }
-      
-  
-  //     // return solution;
-  //   }
-  
-  //   // solution = findSolutionCount(0,0);
-  //   console.log('Number of solutions for ' + n + ' queens:', solution);
-  //   return solution;
-  // };
